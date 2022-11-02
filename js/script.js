c6c2f9b1b101689ff9bb96d12d61d0cb1b7398a1
Francescolapiana <Lushika1219@gmail.com>
2022-11-02 15:34:50 +0100

    Start

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const array = [
+    {
+       
+    },
+    {
+       
+    },
+    {
+       
+    },
+    {
+        
+    },
+]
+
+console.log(array);
+
+const {createApp} = Vue;
+const app = createApp({
+    data(){
+        return {
+            
+        }
+    },
+
+    methods: {
+    },
+
+    computed: {
+    },
+
+    mounted(){
+    },
+
+    created(){
+    },
+
+    beforeCreate(){
+    },
+
+});
+app.mount('#foil')
